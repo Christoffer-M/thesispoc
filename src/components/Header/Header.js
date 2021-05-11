5c0fd6e4a8860ba03e36511a196e102728f14855
Christoffer Ry Mouritzen <cmou@itu.dk>
2021-05-11 11:20:17 +0200

    fix deployment issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,8 @@ import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropDown";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+
 import { isMobile } from "react-device-detect";
 
 const Header = () => {
